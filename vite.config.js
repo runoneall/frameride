49f6b183c46980bcae9e23652733cfb8fd6eb9ad
Your Name <you@example.com>
2026-04-14 09:00:01 +0800

    配置 vite-plugin-monaco-editor-esm

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,13 @@ import { defineConfig } from 'vite'
 import path from 'node:path'
 import electron from 'vite-plugin-electron/simple'
 import vue from '@vitejs/plugin-vue'
+import monacoEditorPlugin from 'vite-plugin-monaco-editor-esm'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         vue(),
+        monacoEditorPlugin({}),
         electron({
             main: {
                 // Shortcut of `build.lib.entry`.
