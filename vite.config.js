feda62e9614392117cce93fb0bde083c732ac5db
Your Name <you@example.com>
2026-04-15 10:22:19 +0800

    清理依赖

--- vite.config.js
+++ vite.config.js
@@ -6,29 +6,21 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
     plugins: [
         vue(),
+
         electron({
             main: {
                 entry: 'electron/main.mjs'
             },
+
             preload: {
                 input: path.join(__dirname, 'electron/preload.mjs')
             },
+
             renderer: process.env.NODE_ENV === 'test' ? undefined : {}
         })
     ],
-    optimizeDeps: {
-        exclude: ['monaco-editor']
-    },
+
     build: {
-        target: 'es2022',
-        rollupOptions: {
-            output: {
-                manualChunks: id => {
-                    if (id.includes('monaco-editor')) {
-                        return 'monaco-editor'
-                    }
-                }
-            }
-        }
+        target: 'es2022'
     }
 })
