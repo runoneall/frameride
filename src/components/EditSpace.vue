d23965eb67571807db8b0afa60a03cd642d08c5d
Your Name <you@example.com>
2026-04-16 09:22:36 +0800

    添加一个编辑器

--- src/components/EditSpace.vue
+++ src/components/EditSpace.vue
@@ -1,5 +1,14 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+import { CodeEditor } from 'monaco-editor-vue3'
 
-<template></template>
+const code = ref(`function hello() {
+  console.log('Hello Monaco Editor Vue3!');
+}`)
+</script>
+
+<template>
+    <CodeEditor v-model:value="code" language="javascript" theme="vs-dark" :height="400" />
+</template>
 
 <style scoped></style>
